ca7d55eeb05aead9e4645e48b6fe47d470cbf1bc
gloryarz <gloryarz@gmail.com>
2018-08-19 19:23:36 -0500

    Se inicia firebase storage y firecast

---
+++ src/js/firebase.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDRnt-RINN9TA3B-04CVHQPoPmvbx8KwpQ",
+    authDomain: "bitacora-94f01.firebaseapp.com",
+    databaseURL: "https://bitacora-94f01.firebaseio.com",
+    projectId: "bitacora-94f01",
+    storageBucket: "bitacora-94f01.appspot.com",
+    messagingSenderId: "10160378360"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.firestore(); // Firestore
+  var storage = firebase.storage(); // Storage
